e74ea8d8c56a2298f06784429a8cf692bf628441
AMUNIM <abdulmunim2005@gmail.com>
2019-01-17 15:19:44 +0300

    Can Successfully download json from webpages to egt location and timings of prayers from the internet

--- app/src/main/java/com/amfk/prayer_alert/MainActivity.java
+++ app/src/main/java/com/amfk/prayer_alert/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amfk.prayer_alert;
 
+import java.io.IOException;
 import java.net.*;
 
 import android.content.Context;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse {
     private static final String IPSTACK_KEY = "7d4df146a6335f13540003cba6cb5da7";
@@ -51,12 +53,11 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
     public void processFinish(String output) {
         //Here you will receive the result fired from async class
         //of onPostExecute(result) method.
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().create();
 
         if (gettingLocation) {
             gettingLocation = false;
-            IPStack stack = new IPStack();
-            gson.fromJson(output, stack.getClass());
+            IPStack stack = gson.fromJson(output, IPStack.class);
 
             GetTimings(stack);
         } else {
