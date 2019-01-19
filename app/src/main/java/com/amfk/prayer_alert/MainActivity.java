53271b557e09b985f165440ae5f3506885ae5245
AMUNIM <abdulmunim2005@gmail.com>
2019-01-19 10:47:50 +0300

    Now save button will set alarm of 1 minute (Untested)

--- app/src/main/java/com/amfk/prayer_alert/MainActivity.java
+++ app/src/main/java/com/amfk/prayer_alert/MainActivity.java
@@ -1,19 +1,17 @@
 package com.amfk.prayer_alert;
 
-import java.io.IOException;
-import java.net.*;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
-import android.net.ConnectivityManager;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import org.w3c.dom.Text;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private static EditText Asr;
     private static EditText Maghrib;
     private static EditText Isha;
+
+    private static Namaaz Timings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         Asr.setText(namaaz.data.timings.asr);
         Maghrib.setText(namaaz.data.timings.maghrib);
         Isha.setText(namaaz.data.timings.isha);
+
+        Timings = namaaz;
     }
 
     private void SetTextReferences(){
@@ -61,5 +63,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //Set alarms of the timings
+        SetAlarm(Timings);
+    }
+
+    private void SetAlarm(Namaaz namaaz){
+        //Retrieve a pending intent that will perform the broadcast
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,0,alarmIntent,0);
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        //calendar.set(Calendar.HOUR_OF_DAY, TIME OF DHUR);
+        calendar.add(Calendar.MINUTE, 1);
+
+        //set alarm
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 8000, pendingIntent);
     }
 }
